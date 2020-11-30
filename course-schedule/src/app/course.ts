a1360dcd63a672ca0bf199b08044f77d51da0474
Dano Rawlins <dano.hyde@gmail.com>
2020-11-30 17:15:22 -0600

    Header content set and styled

--- course-schedule/src/app/course.ts
+++ course-schedule/src/app/course.ts
@@ -1,2 +1,17 @@
 export class Course {
+    title: string;
+    code: string;
+    days: string[];
+    startTime: string;
+    endTime: string;
+    credits: number;
+
+    constructor(title: string, code: string, days: string[], startTime: string, endTime: string, credits: number) {
+        this.title= title;
+        this.code=code;
+        this.days= days;
+        this.startTime= startTime;
+        this.endTime= endTime;
+        this.credits= credits;
+    };
 }
